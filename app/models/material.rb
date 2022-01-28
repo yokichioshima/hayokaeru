9934f3607526aa0236b20aaf9a3fa4a938709632
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-01-28 23:39:47 +0900

    モデル、テーブル、コントローラー作成
    materialモデル
    materialsテーブル
    materialsコントローラー

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,6 @@
 class Material < ApplicationRecord
+  validates :name, presence: true
+  validates :category_id, presence: true
+
+  has_one_attached :image
 end
--- db/migrate/20220128141930_create_materials.rb
+++ db/migrate/20220128141930_create_materials.rb
@@ -1,7 +1,10 @@
 class CreateMaterials < ActiveRecord::Migration[6.0]
   def change
     create_table :materials do |t|
-
+      t.string :name, null: false
+      t.integer :category_id, null: false
+      t.integer :price
+      t.string :options
       t.timestamps
     end
   end
