102fab492bde1570ce8f4310d76cb1a1eb42bdba
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-01-29 00:05:42 +0900

    食材一覧ページルーティング実装

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,2 +1,11 @@
 class MaterialsController < ApplicationController
+  def index
+    @materials_2 = Material.where(category_id: 2)
+    @materials_3 = Material.where(category_id: 3)
+    @materials_4 = Material.where(category_id: 4)
+    @materials_5 = Material.where(category_id: 5)
+    @materials_6 = Material.where(category_id: 6)
+    @materials_7 = Material.where(category_id: 7)
+    @materials_8 = Material.where(category_id: 8)
+  end
 end
