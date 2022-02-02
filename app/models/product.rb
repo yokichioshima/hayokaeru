fa8f8c3af5ed25d0b56c3b3ff0e44e5755226e87
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-02-02 23:38:07 +0900

    productsコントローラー作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :sort_id
+  belongs_to :sort
   
   validates :image, presence: true
   validates :name, presence: true
   validates :sort_id, numericality: { other_than: 1 , message: "can't be blank" }
 
   has_one_attached :image
-  has_many :materials through: :product_materials
+  # has_many :materials through: :product_materials
 end
