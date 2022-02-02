fda0dca5a73536b4aaee91166e80b0b61ba89104
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-02-02 17:49:53 +0900

    編集機能実装

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -16,6 +16,15 @@ class MaterialsController < ApplicationController
     end
   end
 
+  def edit
+    @material = Material.find(params[:id])
+  end
+
+  def update
+    material = Material.find(params[:id])
+    material.update(material_params)
+  end
+
   def destroy
     material = Material.find(params[:id])
     material.destroy
