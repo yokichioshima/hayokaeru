8e05882b7ae56e539c2ff3e31c35d53025bfddc5
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-02-09 02:06:08 +0900

    turbolinks無効化、rubocop導入、実行

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :sort
-  
+
   validates :image, presence: true
   validates :name, presence: true
-  validates :sort_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :sort_id, numericality: { other_than: 1, message: "can't be blank" }
 
   has_one_attached :image
   has_many :product_materials, dependent: :destroy
