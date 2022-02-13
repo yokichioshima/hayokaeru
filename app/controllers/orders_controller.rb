1d59f9ff2fe9f3aa81823560fa632231bd650bc0
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-02-14 08:53:32 +0900

    first commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,16 @@ class OrdersController < ApplicationController
 
   def create
     binding.pry
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      redirect_to action: :new
+    end
   end
 
   private
+  def order_params
+    params.permit(:name, :product_1_id, :product_2_id, :product_3_id, :product_4_id, :product_5_id)
+  end
 end
