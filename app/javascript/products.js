e7c19c201aee860dee3fd9d9614fa9b7f9bcd40d
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-02-16 00:18:18 +0900

    product新規登録javascriptリファクタリング

--- app/javascript/products.js
+++ app/javascript/products.js
@@ -1,23 +1,33 @@
-function readyForGettingMaterialIds (){
-  const materialElements = document.querySelectorAll(".material");
-  const pickMaterials = document.getElementById("pick-materials");
-  
-
-  materialElements.forEach(function(list) {
-    list.addEventListener('click', function(){
-      const materialName = list.querySelector(".material-name");
-      const materialId = list.querySelector(".material-id");
+/* 食材画像クリックイベント発火動作定義 */
+const inputMaterialByClickingImage = (materials, pickMaterials) => {
+  materials.forEach(function(material) {
+    material.addEventListener('click', function(){
+      const materialName = material.querySelector(".material-name");
+      const materialId = material.querySelector(".material-id");
       const htmlPickMaterialNames = `
-        <b id="${materialName.innerHTML}">
+        <div id="${materialName.innerHTML}">
           ${materialName.innerHTML}
-      </b>`;
+        </div>`;
       const htmlPickMaterialIds = `
         <input name="[material_ids][]" type="hidden" class="input-form" id="${materialId.innerHTML}" value=${materialId.innerHTML} > 
-      `;  
+        `;  
       pickMaterials.insertAdjacentHTML("beforeend", htmlPickMaterialNames);
       pickMaterials.insertAdjacentHTML("afterend", htmlPickMaterialIds);
     });
   });
 };
+/* 食材画像クリックイベント発火動作定義 */
 
+function readyForGettingMaterialIds (){
+  /* 入力情報元の要素取得 */
+  const materials = document.querySelectorAll(".material");
+  /* 入力情報元の要素取得 */
+
+  /* 食材名表示BOX取得 */
+  const pickMaterials = document.getElementById("pick-materials");
+  /* 食材名表示BOX取得 */
+
+  inputMaterialByClickingImage(materials, pickMaterials);
+};
+  
 window.addEventListener('load', readyForGettingMaterialIds);
