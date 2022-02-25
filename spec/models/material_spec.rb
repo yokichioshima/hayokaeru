e91c25b39a487dd81778d87e7d2a3b3f3efb9827
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-02-25 18:48:15 +0900

    productモデル, orderモデル単体テスト実装

--- spec/models/material_spec.rb
+++ spec/models/material_spec.rb
@@ -10,16 +10,7 @@ RSpec.describe Material, type: :model do
       it 'すべての項目を入力すれば保存できる' do
         expect(@material).to be_valid
       end
-      it '価格が空でも保存できる' do
-        @material.price = ''
-        expect(@material).to be_valid
-      end
       it '追加事項が空でも保存できる' do
-        @material.options = ''
-        expect(@material).to be_valid
-      end
-      it '価格と追加事項が空でも保存できる' do
-        @material.price = ''
         @material.options = ''
         expect(@material).to be_valid
       end
@@ -29,17 +20,22 @@ RSpec.describe Material, type: :model do
       it '名前が空では保存できない' do
         @material.name = ''
         @material.valid?
-        expect(@material.errors.full_messages).to include("Name can't be blank")
+        expect(@material.errors.full_messages).to include("食材名を入力してください")
       end
       it '画像がなければ保存できない' do
         @material.image = nil
         @material.valid?
-        expect(@material.errors.full_messages).to include("Image can't be blank")
+        expect(@material.errors.full_messages).to include("食材画像を入力してください")
       end
-      it 'category_idが1では保存できない' do
+      it '基礎食品群が -- では保存できない' do
         @material.category_id = 1
         @material.valid?
-        expect(@material.errors.full_messages).to include("Category can't be blank")
+        expect(@material.errors.full_messages).to include("基礎食品群を入力してください")
+      end
+      it '分量の単位が空では保存できない' do
+        @material.quantity_unit = ''
+        @material.valid?
+        expect(@material.errors.full_messages).to include("分量の単位を入力してください")
       end
     end
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  describe '献立の登録' do
+    context '献立が登録できる場合' do
+      it '献立名があれば登録できる' do
+        expect(@order).to be_valid
+      end
+    end
+    context '献立が登録できない場合' do
+      it '献立名が空では登録できない' do
+        @order.name = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("献立の名前を入力してください")
+      end
+    end
+  end
 end
