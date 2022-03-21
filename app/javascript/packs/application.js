53ecc5e7f5e95f142a6919f598965061e783d8bb
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-03-21 20:29:14 +0900

    人数入力機能実装、header position fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../products")
 require("../orders")
+require("../show_summary")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
