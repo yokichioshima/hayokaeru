99689fa1a6eaf99f2baf4747907a9b5adfefc813
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-04-30 23:36:48 +0900

    インデント半角2から半角4へ変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,57 +1,57 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:edit, :update, :destroy]
-  before_action :set_product_all, only: [:new, :edit]
+    before_action :set_order, only: [:edit, :update, :destroy]
+    before_action :set_product_all, only: [:new, :edit]
 
-  def index
-    @orders = Order.all
-  end
-
-  def new
-    @order = Order.new
-  end
-
-  def create
-    @order = Order.new(order_params)
-    if @order.save
-      redirect_to root_path
-    else
-      @products = Product.all
-      render :new
+    def index
+        @orders = Order.all
     end
-  end
 
-  def edit
-  end
-
-  def update
-    if @order.update(order_params)
-      redirect_to root_path
-    else
-      @products = Product.all
-      render :edit
+    def new
+        @order = Order.new
     end
-  end
 
-  def destroy
-    @order.destroy
-    redirect_to root_path
-  end
+    def create
+        @order = Order.new(order_params)
+        if @order.save
+            redirect_to root_path
+        else
+            @products = Product.all
+            render :new
+        end
+    end
 
-  def show_summary
-    @orders = Order.includes(products: :materials, products: :product_materials)
-  end
+    def edit
+    end
 
-  private
+    def update
+        if @order.update(order_params)
+            redirect_to root_path
+        else
+            @products = Product.all
+            render :edit
+        end
+    end
 
-  def order_params
-    params.require(:order).permit(:name, product_ids: [])
-  end
+    def destroy
+        @order.destroy
+        redirect_to root_path
+    end
 
-  def set_order
-    @order = Order.find(params[:id])
-  end
+    def show_summary
+        @orders = Order.includes(products: :materials, products: :product_materials)
+    end
 
-  def set_product_all
-    @products = Product.all
-  end
+    private
+
+    def order_params
+        params.require(:order).permit(:name, product_ids: [])
+    end
+
+    def set_order
+        @order = Order.find(params[:id])
+    end
+
+    def set_product_all
+        @products = Product.all
+    end
 end
