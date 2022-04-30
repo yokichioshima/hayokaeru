d149edbb9e783678337d1ca6cc724b6b4c326868
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-04-30 23:42:42 +0900

    インデント半角2から半角4へ変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,68 +1,68 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :set_material_all, only: [:new, :edit]
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
+    before_action :set_material_all, only: [:new, :edit]
 
-  def index
-    @products = Product.all
-  end
-
-  def show
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      @product.product_materials.each_with_index do |product_material, i|
-        product_material.save_single_quantity(product_material, single_quantities_params[:single_quantities][i])
-      end
-      redirect_to action: :index
-    else
-      @materials = Material.all
-      render :new
+    def index
+        @products = Product.all
     end
-  end
 
-  def edit
-    @product_materials = ProductMaterial.where(product_id: @product.id)
-  end
-
-  def update
-    if @product.update(product_params)
-      @product.product_materials.each_with_index do |product_material, i|
-        product_material.save_single_quantity(product_material, single_quantities_params[:single_quantities][i])
-      end
-      redirect_to action: :show
-    else
-      @materials = Material.all
-      @product_materials = @product.product_materials
-      render :edit
+    def show
     end
-  end
 
-  def destroy
-    @product.destroy
-    redirect_to action: :index
-  end
+    def new
+        @product = Product.new
+    end
 
-  private
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            @product.product_materials.each_with_index do |product_material, i|
+                product_material.save_single_quantity(product_material, single_quantities_params[:single_quantities][i])
+            end
+            redirect_to action: :index
+        else
+            @materials = Material.all
+            render :new
+        end
+    end
 
-  def product_params
-    params.require(:product).permit(:name, :image, :sort_id, :options, material_ids: [])
-  end
+    def edit
+        @product_materials = ProductMaterial.where(product_id: @product.id)
+    end
 
-  def single_quantities_params
-    params.permit(single_quantities: [])
-  end
+    def update
+        if @product.update(product_params)
+            @product.product_materials.each_with_index do |product_material, i|
+                product_material.save_single_quantity(product_material, single_quantities_params[:single_quantities][i])
+            end
+            redirect_to action: :show
+        else
+            @materials = Material.all
+            @product_materials = @product.product_materials
+            render :edit
+        end
+    end
 
-  def set_product
-    @product = Product.find(params[:id])
-  end
+    def destroy
+        @product.destroy
+        redirect_to action: :index
+    end
 
-  def set_material_all
-    @materials = Material.all
-  end
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :image, :sort_id, :options, material_ids: [])
+    end
+
+    def single_quantities_params
+        params.permit(single_quantities: [])
+    end
+
+    def set_product
+        @product = Product.find(params[:id])
+    end
+
+    def set_material_all
+        @materials = Material.all
+    end
 end
