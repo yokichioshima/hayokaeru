8af75a9391ec86c0d2b6b451bdd76ff673793842
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-04-30 23:46:10 +0900

    インデント半角2から半角4へ変更

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,20 +1,20 @@
 module OrdersHelper
-  # 献立に使用されている食材と分量をそれぞれ配列に格納する
-  def collect_materials(orders)
-    materials = []
-    quantities = {}
-    orders.each do |order|
-      order.products.each do |product|
-        product.materials.each do |material|
-          materials << material
-          quantities[material.id] = 0 if quantities[material.id].nil?
-          quantities[material.id] += product.product_materials.select do |hash|
-                                       hash.material_id == material.id
-                                     end [0].single_quantity
+    # 献立に使用されている食材と分量をそれぞれ配列に格納する
+    def collect_materials(orders)
+        materials = []
+        quantities = {}
+        orders.each do |order|
+            order.products.each do |product|
+                product.materials.each do |material|
+                    materials << material
+                    quantities[material.id] = 0 if quantities[material.id].nil?
+                    quantities[material.id] += product.product_materials.select do |hash|
+                                                    hash.material_id == material.id
+                                                end [0].single_quantity
+                end
+            end
         end
-      end
+        materials = materials.uniq
+        [materials, quantities]
     end
-    materials = materials.uniq
-    [materials, quantities]
-  end
 end
