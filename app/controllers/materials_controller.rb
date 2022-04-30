1750759c9bcea8f82deac3769488aeece7e92dc1
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-04-30 23:31:30 +0900

    インデントの間隔を半角2から半角4へ変更

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,46 +1,46 @@
 class MaterialsController < ApplicationController
-  before_action :set_material, only: [:edit, :update, :destroy]
+    before_action :set_material, only: [:edit, :update, :destroy]
 
-  def index
-    @materials = Material.all
-  end
-
-  def new
-    @material = Material.new
-  end
-
-  def create
-    @material = Material.new(material_params)
-    if @material.save
-      redirect_to action: :index
-    else
-      render :new
+    def index
+        @materials = Material.all
     end
-  end
 
-  def edit
-  end
-
-  def update
-    if @material.update(material_params)
-      redirect_to action: :index
-    else
-      render :edit
+    def new
+        @material = Material.new
     end
-  end
 
-  def destroy
-    @material.destroy
-    redirect_to action: :index
-  end
+    def create
+        @material = Material.new(material_params)
+        if @material.save
+            redirect_to action: :index
+        else
+            render :new
+        end
+    end
 
-  private
+    def edit
+    end
 
-  def material_params
-    params.require(:material).permit(:name, :image, :category_id, :quantity_unit, :options)
-  end
+    def update
+        if @material.update(material_params)
+            redirect_to action: :index
+        else
+            render :edit
+        end
+    end
 
-  def set_material
-    @material = Material.find(params[:id])
-  end
+    def destroy
+        @material.destroy
+        redirect_to action: :index
+    end
+
+    private
+
+    def material_params
+        params.require(:material).permit(:name, :image, :category_id, :quantity_unit, :options)
+    end
+
+    def set_material
+        @material = Material.find(params[:id])
+    end
 end
