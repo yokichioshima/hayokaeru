5c19a2d9b24c28c8a8c598b80ce56bd655c21218
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-05-01 00:00:14 +0900

    インデント半角2から半角4へ変更

--- app/javascript/products.js
+++ app/javascript/products.js
@@ -1,29 +1,28 @@
 /* 食材画像クリックイベント発火動作定義 */
 const inputMaterialByClickingImage = (materials, pickMaterials, guidTextOptions) => {
-  materials.forEach(function(material) {
-    material.addEventListener('click', function(){
-      const materialName = material.querySelector(".material-name");
-      const materialId = material.querySelector(".material-id");
-      const materialQuantityUnit = material.querySelector(".material-quantity-unit");
-      const inputNameBox = document.getElementById(`${materialName.innerHTML}`);
-      const inputForm = document.getElementById(`${materialId.innerHTML}`);
-      if ( inputNameBox != null || inputForm != null ) {
-        inputNameBox.remove();
-        inputForm.remove();
-      } else {
-      const htmlPickMaterialNames = `
-        <div class="material-name-box" id="${materialName.innerHTML}">
-          ${materialName.innerHTML}<input name="[single_quantities][]" class="input-quantity-form" 
-          id="quantity-${materialId.innerHTML}" ><div class="material-quantity-unit-on-form">${materialQuantityUnit.innerHTML}</div>
-        </div>`;
-      const htmlPickMaterialIds = `
-        <input name="product[material_ids][]" type="hidden" class="input-form" id="${materialId.innerHTML}" value=${materialId.innerHTML} > 
-        `;  
-      pickMaterials.insertAdjacentHTML("beforeend", htmlPickMaterialNames);
-      guidTextOptions.insertAdjacentHTML("beforebegin", htmlPickMaterialIds);
-      };
+    materials.forEach(function(material) {
+        material.addEventListener('click', function(){
+            const materialName = material.querySelector(".material-name");
+            const materialId = material.querySelector(".material-id");
+            const materialQuantityUnit = material.querySelector(".material-quantity-unit");
+            const inputNameBox = document.getElementById(`${materialName.innerHTML}`);
+            const inputForm = document.getElementById(`${materialId.innerHTML}`);
+            if ( inputNameBox != null || inputForm != null ) {
+                inputNameBox.remove();
+                inputForm.remove();
+            } else {
+                const htmlPickMaterialNames = `
+                <div class="material-name-box" id="${materialName.innerHTML}">
+                ${materialName.innerHTML}<input name="[single_quantities][]" class="input-quantity-form" 
+                id="quantity-${materialId.innerHTML}" ><div class="material-quantity-unit-on-form">${materialQuantityUnit.innerHTML}</div>
+                </div>`;
+                const htmlPickMaterialIds = `
+                <input name="product[material_ids][]" type="hidden" class="input-form" id="${materialId.innerHTML}" value=${materialId.innerHTML} > `;  
+                pickMaterials.insertAdjacentHTML("beforeend", htmlPickMaterialNames);
+                guidTextOptions.insertAdjacentHTML("beforebegin", htmlPickMaterialIds);
+            };
+        });
     });
-  });
 };
 /* 食材画像クリックイベント発火動作定義 */
 
@@ -56,17 +55,17 @@ const inputMaterialByClickingImage = (materials, pickMaterials, guidTextOptions)
 /* サイドバー料理名、id入力欄の入れ替え */
 
 function readyForGettingMaterialIds (){
-  /* 入力情報元の要素取得 */
-  const materials = document.querySelectorAll(".material");
-  /* 入力情報元の要素取得 */
+    /* 入力情報元の要素取得 */
+    const materials = document.querySelectorAll(".material");
+    /* 入力情報元の要素取得 */
 
-  /* 食材名表示BOX取得 */
-  const pickMaterials = document.getElementById("pick-materials");
-  /* 食材名表示BOX取得 */
+    /* 食材名表示BOX取得 */
+    const pickMaterials = document.getElementById("pick-materials");
+    /* 食材名表示BOX取得 */
 
-  const guidTextOptions = document.getElementById("guid-text-options");
+    const guidTextOptions = document.getElementById("guid-text-options");
 
-  inputMaterialByClickingImage(materials, pickMaterials, guidTextOptions);
+    inputMaterialByClickingImage(materials, pickMaterials, guidTextOptions);
 };
   
 window.addEventListener('load', readyForGettingMaterialIds);
