07a0cfc85780ecdf8b1fe29fd85c017bf457b5f2
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-05-01 00:06:17 +0900

    インデント半角2から半角4へ変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,16 @@
 class Category < ActiveHash::Base
-  self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '肉類・魚類・大豆製品' },
-    { id: 3, name: '乳製品・海藻・小魚類' },
-    { id: 4, name: '緑黄色野菜' },
-    { id: 5, name: '淡色野菜・果物' },
-    { id: 6, name: '穀類・芋類・砂糖' },
-    { id: 7, name: '油脂類' },
-    { id: 8, name: '調味料' },
-    { id: 9, name: 'その他' }
-  ]
+    self.data = [
+        { id: 1, name: '--' },
+        { id: 2, name: '肉類・魚類・大豆製品' },
+        { id: 3, name: '乳製品・海藻・小魚類' },
+        { id: 4, name: '緑黄色野菜' },
+        { id: 5, name: '淡色野菜・果物' },
+        { id: 6, name: '穀類・芋類・砂糖' },
+        { id: 7, name: '油脂類' },
+        { id: 8, name: '調味料' },
+        { id: 9, name: 'その他' }
+    ]
 
-  include ActiveHash::Associations
-  has_many :materials
+    include ActiveHash::Associations
+    has_many :materials
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,12 +1,12 @@
 class Material < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :category_id, numericality: { other_than: 1, message: 'を入力してください' }
-  validates :quantity_unit, presence: true
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    validates :image, presence: true
+    validates :name, presence: true
+    validates :category_id, numericality: { other_than: 1, message: 'を入力してください' }
+    validates :quantity_unit, presence: true
 
-  has_one_attached :image
-  has_many :products, through: :product_materials
-  has_many :product_materials
+    has_one_attached :image
+    has_many :products, through: :product_materials
+    has_many :product_materials
 end
