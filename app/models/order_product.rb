9e97e8e73aa76c5b19969b2a98e39d745f4406d4
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-05-01 00:08:22 +0900

    インデント半角2から半角4へ変更

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,13 +1,13 @@
 class OrderProduct < ApplicationRecord
-  Max_Order_Products_Count = 5
-  belongs_to :product
-  belongs_to :order
+    Max_Order_Products_Count = 5
+    belongs_to :product
+    belongs_to :order
 
-  validate :limit_order_products_count
+    validate :limit_order_products_count
 
-  private
+    private
 
-  def limit_order_products_count
-    errors.add(:base, "#{Max_Order_Products_Count}品までです") if order.products.count >= Max_Order_Products_Count
-  end
+    def limit_order_products_count
+        errors.add(:base, "#{Max_Order_Products_Count}品までです") if order.products.count >= Max_Order_Products_Count
+    end
 end
